84bd13bb6e68f6b5285280d14f2a7f1145956e27
Alexander Tejeda <AlexanderTejedaBarahona10@gmail.com>
2024-07-02 23:26:26 -0600

    Se modifico el servicio para finalizar el header del landing page

--- src/app/modules/petwebsite/components/header/header.component.ts
+++ src/app/modules/petwebsite/components/header/header.component.ts
@@ -1,9 +1,18 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit{
+
+	@ViewChild('inputSearchBreed') inputSearch! : ElementRef; 
+
+	ngAfterViewInit(): void {
+
+		this.inputSearch.nativeElement.setAttribute('size', this.inputSearch.nativeElement.getAttribute('placeholder').length)
+		
+	}
+
 }
