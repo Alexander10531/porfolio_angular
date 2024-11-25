83de0600911dcc47075de7ad9581c072f672c0b4
Alexander Tejeda <AlexanderTejedaBarahona10@gmail.com>
2024-11-24 22:47:36 -0600

    Modificacion de servicio para agregar countdown

--- src/app/modules/landing-page/components/login/login.component.ts
+++ src/app/modules/landing-page/components/login/login.component.ts
@@ -8,8 +8,36 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
-  public LoginComponent(){
-    console.log("Se esta haciendo iniciando")
+  days : string = "00"; 
+  hours : string = "00"; 
+  minutes : string = "00"; 
+  seconds : string = "00"; 
+  countDownDate : number = new Date("Dec 2, 2024 00:00:00").getTime();
+
+  constructor(){
+
+    let interval = setInterval(() => {
+      
+      let now : number = new Date().getTime();
+      let distance = this.countDownDate - now; 
+
+      this.days = Math.floor(distance / (1000 * 60 * 60 * 24)).toString();
+      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString();
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString();
+      this.seconds = Math.floor((distance % (1000 * 60)) / 1000).toString();
+      
+      this.days = this.formatTime(this.days);
+      this.hours = this.formatTime(this.hours); 
+      this.minutes = this.formatTime(this.minutes); 
+      this.seconds = this.formatTime(this.seconds); 
+
+
+    }, 1000)
+
+  }
+
+  public formatTime(fecha : string) : string{
+    return fecha.length == 1 ? `0${fecha}` : fecha;   
   }
 
 }
